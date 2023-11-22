a321beef66f6af94c63db197ec23f139a5e28b1e
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-22 04:25:12 -0500

    Install tailwind css and style splash screen

---
+++ config/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    colors: {
+      main: "#3778c2",
+      second: "#5fb523",
+      text: "#434b54",
+      white: "#ffffff",
+      gray: "#9ca3af",
+      second_gray: "#f3f4f6",
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
